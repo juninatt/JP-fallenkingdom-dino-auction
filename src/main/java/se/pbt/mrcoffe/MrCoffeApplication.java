c4c73a9d4c62da67f8ae5ccc8d4686f2ffa5989a
Petter B <petter.bergstrom@protonmail.com>
2023-06-10 22:33:23 +0200

    Initial commit

---
+++ src/main/java/se/pbt/mrcoffe/MrCoffeApplication.java
@@ -0,0 +1,13 @@
+package se.pbt.mrcoffe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrCoffeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrCoffeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/pbt/mrcoffe/MrCoffeApplicationTests.java
@@ -0,0 +1,13 @@
+package se.pbt.mrcoffe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrCoffeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
