3827c7c1673af73ae1f385bc6d4dd55533dddfd3
Petter B <petter.bergstrom@protonmail.com>
2023-06-11 23:31:27 +0200

    Correct annotation and parameter

--- src/main/java/se/pbt/mrcoffee/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/se/pbt/mrcoffee/exception/handler/GlobalExceptionHandler.java
@@ -1,23 +1,23 @@
 package se.pbt.mrcoffee.exception.handler;
 
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import se.pbt.mrcoffee.exception.CoffeeNotFoundException;
 
 import java.time.Instant;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
-    public ResponseEntity<ErrorResponse<String>> handleNotFoundException(ChangeSetPersister.NotFoundException exception) {
-        ErrorResponse<String> errorResponse = new ErrorResponse<>(
+    @ExceptionHandler(CoffeeNotFoundException.class)
+    public ResponseEntity<ErrorResponse<?>> handleCoffeeNotFoundException(CoffeeNotFoundException exception) {
+        ErrorResponse<?> errorResponse = new ErrorResponse<>(
                 HttpStatus.NOT_FOUND.value(),
                 exception.getMessage(),
                 Instant.now().toEpochMilli(),
-                null
+                exception
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
