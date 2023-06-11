2c08035e5c46ae1fa738d51ae10fb8782e88b78a
Petter B <petter.bergstrom@protonmail.com>
2023-06-11 19:47:16 +0200

    Override findAll() in CoffeeRepository.java

--- src/main/java/se/pbt/mrcoffee/repository/CoffeeRepository.java
+++ src/main/java/se/pbt/mrcoffee/repository/CoffeeRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import se.pbt.mrcoffee.model.Coffee;
 
+import java.util.List;
+
 @Repository
 public interface CoffeeRepository extends CrudRepository<Coffee, Long> {
+    @Override
+    List<Coffee> findAll();
 }
