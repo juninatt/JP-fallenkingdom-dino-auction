037e278a5c4c715ba0910fe368ba2f9e294d944a
Petter B <petter.bergstrom@protonmail.com>
2023-06-11 17:07:54 +0200

    Fix miss spelling

--- src/main/java/se/pbt/mrcoffee/MrCoffeeApplication.java
+++ src/main/java/se/pbt/mrcoffee/MrCoffeeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MrCoffeApplication {
+public class MrCoffeeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MrCoffeApplication.class, args);
+		SpringApplication.run(MrCoffeeApplication.class, args);
 	}
 
 }
