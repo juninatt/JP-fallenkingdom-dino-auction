de8259fae0e8885bb56132d518f70db4bb9d17ab
Petter B <petter.bergstrom@protonmail.com>
2023-06-11 17:40:23 +0200

    Add TestObjectFactory.java

---
+++ src/test/java/se/pbt/mrcoffee/testobject/TestObjectFactory.java
@@ -0,0 +1,19 @@
+package se.pbt.mrcoffee.testobject;
+
+import se.pbt.mrcoffee.model.Coffee;
+
+import java.math.BigDecimal;
+
+public class TestObjectFactory {
+
+    public static Coffee createCoffee(String name) {
+        return new Coffee(
+                name,
+                "Description of " + name,
+                BigDecimal.valueOf(9.99),
+                "Origin of " + name,
+                "Roast level of " + name,
+                "Flavour notes of " + name,
+                "Caffeine content of " + name);
+    }
+}
