ad399236e2002348e90941020ef25ee5ac33977a
Petter B <petter.bergstrom@protonmail.com>
2023-06-20 23:25:41 +0200

    Refactor CustomerContact.java
    - rename class
    - remove redundant field annotations
    - override equals and hashCode

--- src/main/java/se/pbt/mrcoffee/model/contact/CustomerContact.java
+++ src/main/java/se/pbt/mrcoffee/model/contact/CustomerContact.java
@@ -1,6 +1,5 @@
 package se.pbt.mrcoffee.model.contact;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
@@ -8,19 +7,19 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import se.pbt.mrcoffee.model.user.MrCoffeeUser;
 
+import java.util.Objects;
+
 /**
  * Represents contact information for a private customer.
  */
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-public class PrivateCustomerContact extends Contact {
+public class CustomerContact extends Contact {
 
-    @Column(name = "first_name")
     @NotBlank(message = "First name is required")
     @Size(max = 25, message = "Cant be longer than 25 characters")
     private String firstName;
 
-    @Column(name = "last_name")
     @NotBlank(message = "Last name is required")
     @Size(max = 30, message = "Cant be longer than 30 characters")
     private String lastName;
@@ -28,7 +27,7 @@ public class PrivateCustomerContact extends Contact {
     /**
      * Default constructor for the PrivateCustomerContact class.
      */
-    public PrivateCustomerContact() {
+    public CustomerContact() {
     }
 
     /**
@@ -41,7 +40,7 @@ public class PrivateCustomerContact extends Contact {
      * @param firstName       The first name of the private customer.
      * @param lastName        The last name of the private customer.
      */
-    public PrivateCustomerContact(String email, String phoneNumber, String additionalInfo, MrCoffeeUser mrCoffeeUser, String firstName, String lastName) {
+    public CustomerContact(String email, String phoneNumber, String additionalInfo, MrCoffeeUser mrCoffeeUser, String firstName, String lastName) {
         super(email, phoneNumber, additionalInfo, mrCoffeeUser);
         this.firstName = firstName;
         this.lastName = lastName;
@@ -62,5 +61,20 @@ public class PrivateCustomerContact extends Contact {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        CustomerContact that = (CustomerContact) o;
+        return Objects.equals(firstName, that.firstName) &&
+                Objects.equals(lastName, that.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), firstName, lastName);
     }
 }
