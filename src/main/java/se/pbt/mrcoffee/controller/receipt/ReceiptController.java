31b6517aba421ccac8176462419750696820083e
Petter B <petter.bergstrom@protonmail.com>
2023-06-23 19:01:17 +0200

    Add @GlobalApiResponses to methods

--- src/main/java/se/pbt/mrcoffee/controller/receipt/ReceiptController.java
+++ src/main/java/se/pbt/mrcoffee/controller/receipt/ReceiptController.java
@@ -1,9 +1,13 @@
 package se.pbt.mrcoffee.controller.receipt;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import se.pbt.mrcoffee.annotation.GlobalApiResponses;
 import se.pbt.mrcoffee.model.receipt.Receipt;
 import se.pbt.mrcoffee.service.receipt.ReceiptService;
 
@@ -15,6 +19,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/receipt")
+@Tag(name = "Receipts", description = "Endpoints for managing receipts")
 public class ReceiptController {
 
     private final ReceiptService receiptService;
@@ -30,6 +35,8 @@ public class ReceiptController {
      * @return ResponseEntity containing the list of all receipts.
      */
     @GetMapping
+    @Operation(summary = "Retrieve all receipts", description = "Retrieve a list of all receipts from the database")
+    @GlobalApiResponses(schemaImplementation = Receipt.class)
     public ResponseEntity<List<Receipt>> getAllReceipts() {
         var receipts = receiptService.getAllReceipts();
         return ResponseEntity.ok(receipts);
@@ -42,7 +49,9 @@ public class ReceiptController {
      * @return ResponseEntity containing the retrieved receipt if found, or a not found response.
      */
     @GetMapping("/{id}")
-    public ResponseEntity<Receipt> getReceiptById(@PathVariable long id) {
+    @Operation(summary = "Retrieve a receipt by ID", description = "Retrieve a specific receipt from the database based on the receipts ID")
+    @GlobalApiResponses(schemaImplementation = Receipt.class)
+    public ResponseEntity<Receipt> getReceiptById( @PathVariable @Parameter(description = "Receipt ID", example = "1") long id) {
         var receipt = receiptService.getReceiptById(id);
         if (receipt != null) {
             return ResponseEntity.ok(receipt);
@@ -58,7 +67,9 @@ public class ReceiptController {
      * @return ResponseEntity containing the created receipt.
      */
     @PostMapping
-    public ResponseEntity<Receipt> createReceipt(@RequestBody Receipt receipt) {
+    @Operation(summary = "Create a new receipt", description = "Create a new receipt and persist it to the database")
+    @GlobalApiResponses(schemaImplementation = Receipt.class)
+    public ResponseEntity<Receipt> createReceipt(@RequestBody @Parameter(description = "Receipt object") Receipt receipt) {
         var createdReceipt = receiptService.createReceipt(receipt);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdReceipt);
     }
@@ -71,7 +82,10 @@ public class ReceiptController {
      * @return ResponseEntity containing the updated receipt if found, or a not found response.
      */
     @PutMapping("/{id}")
-    public ResponseEntity<Receipt> updateReceipt(@PathVariable long id, @RequestBody Receipt receipt) {
+    @Operation(summary = "Update an existing receipt",  description = "Update an existing receipt in the database")
+    @GlobalApiResponses(schemaImplementation = Receipt.class)
+    public ResponseEntity<Receipt> updateReceipt(@PathVariable @Parameter(description = "Receipt ID", example = "1") long id,
+                                                 @RequestBody @Parameter(description = "Updated receipt object") Receipt receipt) {
         var updatedReceipt = receiptService.updateReceipt(id, receipt);
         if (updatedReceipt != null) {
             return ResponseEntity.ok(updatedReceipt);
@@ -87,7 +101,9 @@ public class ReceiptController {
      * @return ResponseEntity indicating the success or failure of the deletion.
      */
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteReceipt(@PathVariable long id) {
+    @Operation(summary = "Delete a receipt by ID")
+    @GlobalApiResponses
+    public ResponseEntity<Void> deleteReceipt(@PathVariable @Parameter(description = "Receipt ID", example = "1") long id) {
         boolean deleted = receiptService.deleteReceipt(id);
         if (deleted) {
             return ResponseEntity.noContent().build();
