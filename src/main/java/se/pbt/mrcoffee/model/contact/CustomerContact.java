c07c5310d1741a9f1e010bb7b63cdbe5646eb8f0
Petter B <petter.bergstrom@protonmail.com>
2023-06-24 12:19:17 +0200

    Refactor CustomerContact.java
    - remove redundant javaDoc
    - Clarify validation messages

--- src/main/java/se/pbt/mrcoffee/model/contact/CustomerContact.java
+++ src/main/java/se/pbt/mrcoffee/model/contact/CustomerContact.java
@@ -10,36 +10,23 @@ import se.pbt.mrcoffee.model.user.MrCoffeeUser;
 import java.util.Objects;
 
 /**
- * Represents contact information for a private customer.
+ * Represents contact information for a customer.
  */
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public class CustomerContact extends Contact {
 
     @NotBlank(message = "First name is required")
-    @Size(max = 25, message = "Cant be longer than 25 characters")
+    @Size(max = 25, message = "First name cant be more than 25 characters")
     private String firstName;
 
     @NotBlank(message = "Last name is required")
-    @Size(max = 30, message = "Cant be longer than 30 characters")
+    @Size(max = 30, message = "Last name cant be more than 30 characters")
     private String lastName;
 
-    /**
-     * Default constructor for the PrivateCustomerContact class.
-     */
-    public CustomerContact() {
-    }
 
-    /**
-     * Constructor for the PrivateCustomerContact class.
-     *
-     * @param email           The email address of the private customer.
-     * @param phoneNumber     The phone number of the private customer.
-     * @param additionalInfo  Additional information about the private customer.
-     * @param mrCoffeeUser            The user associated with the private customer.
-     * @param firstName       The first name of the private customer.
-     * @param lastName        The last name of the private customer.
-     */
+    public CustomerContact() {}
+
     public CustomerContact(String email, String phoneNumber, String additionalInfo, MrCoffeeUser mrCoffeeUser, String firstName, String lastName) {
         super(email, phoneNumber, additionalInfo, mrCoffeeUser);
         this.firstName = firstName;
