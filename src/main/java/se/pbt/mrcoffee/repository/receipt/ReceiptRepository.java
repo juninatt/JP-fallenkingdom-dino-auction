16d207c82d20448975b498cd221fc7c5a69b2b38
Petter B <petter.bergstrom@protonmail.com>
2023-06-26 16:23:44 +0200

    Add @Repository annotation to ReceiptRepository.java

--- src/main/java/se/pbt/mrcoffee/repository/receipt/ReceiptRepository.java
+++ src/main/java/se/pbt/mrcoffee/repository/receipt/ReceiptRepository.java
@@ -1,10 +1,12 @@
 package se.pbt.mrcoffee.repository.receipt;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import se.pbt.mrcoffee.model.receipt.Receipt;
 
 import java.util.List;
 
+@Repository
 public interface ReceiptRepository extends CrudRepository<Receipt, Long> {
 
     @Override
