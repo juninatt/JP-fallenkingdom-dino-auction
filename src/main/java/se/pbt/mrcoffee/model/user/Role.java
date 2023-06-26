941a57a5babf66e036ebb821913f543f700aecf0
Petter B <petter.bergstrom@protonmail.com>
2023-06-26 13:49:47 +0200

    Add Role.java

---
+++ src/main/java/se/pbt/mrcoffee/model/user/Role.java
@@ -0,0 +1,60 @@
+package se.pbt.mrcoffee.model.user;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+
+    @Column(unique = true)
+    private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<MrCoffeeUser> users = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "role_permission",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions;
+
+
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Set<MrCoffeeUser> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<MrCoffeeUser> users) {
+        this.users = users;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+}
+
