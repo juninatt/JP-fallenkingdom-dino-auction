3c427537ee59ed4151e13406526a39c02efb4760
Petter B <petter.bergstrom@protonmail.com>
2023-06-26 11:42:34 +0200

    Add relation to Purchase in Customer.java

--- src/main/java/se/pbt/mrcoffee/model/user/Customer.java
+++ src/main/java/se/pbt/mrcoffee/model/user/Customer.java
@@ -1,26 +1,52 @@
 package se.pbt.mrcoffee.model.user;
 
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import se.pbt.mrcoffee.enums.CustomerType;
+import se.pbt.mrcoffee.enums.CustomerLevel;
+import se.pbt.mrcoffee.model.purchase.Purchase;
 
-@Table(name = "customer")
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Customer extends MrCoffeeUser {
 
-    private CustomerType customerType;
+    private CustomerLevel customerLevel;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Purchase> purchases;
 
     public Customer() {}
 
     public Customer(
-            String username,
-            String password,
-            @NotBlank CustomerType customerType
+            @NotBlank String username,
+            @NotBlank String password,
+            CustomerLevel customerLevel,
+            List<Purchase> purchases
     ) {
         super(username, password);
-        this.customerType = customerType;
+        this.customerLevel = customerLevel;
+        this.purchases = purchases;
     }
 
-    public CustomerType getCustomerType() {
-        return customerType;
+    public CustomerLevel getCustomerLevel() {
+        return customerLevel;
+    }
+
+    public void setCustomerLevel(CustomerLevel customerLevel) {
+        this.customerLevel = customerLevel;
+    }
+
+    public List<Purchase> getPurchases() {
+        return purchases;
+    }
+
+    public void setPurchases(List<Purchase> purchases) {
+        this.purchases = purchases;
+    }
+
+    public void addPurchase(Purchase purchase) {
+        purchases.add(purchase);
     }
 }
