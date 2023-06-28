d6d90e077ac09df592a516b6ec0674a0552883c8
Petter B <petter.bergstrom@protonmail.com>
2023-06-29 00:22:05 +0200

    Add form login and password encoder to SecurityConfig.java
    - add login authentication
    - add default home url
    - add password BCrypt password encoder

--- src/main/java/se/pbt/mrcoffee/security/SecurityConfig.java
+++ src/main/java/se/pbt/mrcoffee/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package se.pbt.mrcoffee.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -12,14 +14,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain web(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/").hasAuthority("ADMIN")
-                        .anyRequest().permitAll()
-
-                );
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
