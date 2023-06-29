2dd54e6d51cda89a61b81bbfe69f7af9ea7b12a2
Petter B <petter.bergstrom@protonmail.com>
2023-06-29 19:56:00 +0200

    Refactor HomeController.java
    - add request mapping
    - add javaDoc

--- src/main/java/se/pbt/mrcoffee/controller/HomeController.java
+++ src/main/java/se/pbt/mrcoffee/controller/HomeController.java
@@ -2,12 +2,22 @@ package se.pbt.mrcoffee.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Controller class for handling requests related to the home page.
+ */
 @Controller
+@RequestMapping(value = "/home")
 public class HomeController {
 
-    @GetMapping("/home")
+    /**
+     * Handles the GET request for the home page.
+     *
+     * @return the view for the home page
+     */
+    @GetMapping()
     public String homePage() {
-        return "home.html";
+        return "homepage.html";
     }
 }
