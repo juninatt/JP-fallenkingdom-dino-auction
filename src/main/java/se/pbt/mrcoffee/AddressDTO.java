6c0ef582223e690ac70bada6819483de88ec8f07
Petter B <petter.bergstrom@protonmail.com>
2023-07-01 21:53:21 +0200

    Add record AddressDTO.java

---
+++ src/main/java/se/pbt/mrcoffee/AddressDTO.java
@@ -0,0 +1,15 @@
+package se.pbt.mrcoffee;
+
+/**
+ * Data Transfer Object for transferring Address data.
+ * <p>
+ * This DTO includes the essential fields of the Address entity and
+ * it's used for transferring data across different layers or systems.
+ */
+public record AddressDTO(Long addressId,
+                         String street,
+                         int streetNumber,
+                         int apartmentNumber,
+                         String city,
+                         String postalCode,
+                         String country) {}
