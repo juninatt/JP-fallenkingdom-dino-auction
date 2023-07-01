9b8a4513d68f133b8944d74ba1938dd792c713de
Petter B <petter.bergstrom@protonmail.com>
2023-07-01 19:25:16 +0200

    Restrict PUT, POST and DELETE calls to ADMIN role in SecurityConfig.java

--- src/main/java/se/pbt/mrcoffee/security/SecurityConfig.java
+++ src/main/java/se/pbt/mrcoffee/security/SecurityConfig.java
@@ -2,27 +2,37 @@ package se.pbt.mrcoffee.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 /**
  * Configuration class for web security.
  */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated())
+                        .requestMatchers(antMatcher(HttpMethod.POST)).hasRole("ADMIN")
+                        .requestMatchers(antMatcher(HttpMethod.PUT)).hasRole("ADMIN")
+                        .requestMatchers(antMatcher(HttpMethod.DELETE)).hasRole("ADMIN")
+                         .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(formLogin -> formLogin
                         .defaultSuccessUrl("/home", true)
                         .permitAll());
 
-        return http.build();
+        return http.csrf(AbstractHttpConfigurer::disable).build();
     }
 
     @Bean
