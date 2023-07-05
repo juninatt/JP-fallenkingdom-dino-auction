0702371fbf221a0b4b04765a6eb545ff80e676fc
Petter B <petter.bergstrom@protonmail.com>
2023-07-05 22:49:58 +0200

    Add CustomerContactMapperTest.java

---
+++ src/test/java/se/pbt/mrcoffee/mapper/CustomerContactMapperTest.java
@@ -0,0 +1,56 @@
+package se.pbt.mrcoffee.mapper;
+
+import org.junit.jupiter.api.Test;
+import se.pbt.mrcoffee.testobject.TestObjectFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CustomerContactMapperTest {
+
+    private final CustomerContactMapper mapper = CustomerContactMapper.INSTANCE;
+
+    @Test
+    public void toCustomerContactDTO() {
+        var testUser = TestObjectFactory.createCustomer();
+        var customerContact = TestObjectFactory.createCustomerContact();
+        customerContact.setUser(testUser);
+
+        var customerContactDTO = mapper.toCustomerContactDTO(customerContact);
+
+        assertEquals(customerContact.getFirstName(), customerContactDTO.firstName());
+        assertEquals(customerContact.getLastName(), customerContactDTO.lastName());
+        assertEquals(customerContact.getEmail(), customerContactDTO.email());
+        assertEquals(customerContact.getPhoneNumber(), customerContactDTO.phoneNumber());
+        assertEquals(customerContact.getAdditionalInfo(), customerContactDTO.additionalInfo());
+    }
+
+    @Test
+    public void toCustomerContact() {
+        var customerContactDTO = TestObjectFactory.createCustomerContactDTO();
+
+        var customerContact = mapper.toCustomerContact(customerContactDTO);
+
+        assertEquals(customerContact.getFirstName(), customerContactDTO.firstName());
+        assertEquals(customerContact.getFirstName(), customerContactDTO.firstName());
+        assertEquals(customerContactDTO.email(), customerContact.getEmail());
+        assertEquals(customerContactDTO.phoneNumber(), customerContact.getPhoneNumber());
+        assertEquals(customerContactDTO.additionalInfo(), customerContact.getAdditionalInfo());
+    }
+
+    @Test
+    void toCustomerContactResponseDTO_ShouldReturnCorrectDTO() {
+        // Arrange
+        var contact = TestObjectFactory.createCustomerContact();
+
+        // Act
+        var dto = mapper.toCustomerContactResponseDTO(contact);
+
+        // Assert
+        assertEquals(contact.getEmail(), dto.email());
+        assertEquals(contact.getPhoneNumber(), dto.phoneNumber());
+        assertEquals(contact.getAdditionalInfo(), dto.additionalInfo());
+
+        // Assert address mapping
+        assertEquals(contact.getAddresses().size(), dto.addresses().size());
+    }
+}
