1bd90cbd8bdb2e25fb836eca3995ac3ba75ae515
Petter B <petter.bergstrom@protonmail.com>
2023-09-14 14:43:23 +0200

    Add maven testing plugins (surefire and failsafe) pom file

--- src/main/java/se/pbt/mrcoffee/MrCoffeeApplication.java
+++ src/main/java/se/pbt/mrcoffee/MrCoffeeApplication.java
@@ -3,6 +3,9 @@ package se.pbt.mrcoffee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TODO: Document end points in JSON
+// TODO: Implement Thymeleaf
+
 // TODO: Add validation before object creation
 // TODO: Remove calls to ContactService in CustomerContactControllerTest
 // TODO: Introduce constants for appropriate fields (for ex payment method in Payment)
